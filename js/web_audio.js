5a536c9e3aa2525de5c1e7118a6beb9c392f2404
Dan Proudfoot <dan.proudfoot.iw@gmail.com>
2015-05-04 04:00:01 +0100

    More audio-y stuff, using jqueryUIfor some stuff, because meh

--- js/web_audio.js
+++ js/web_audio.js
@@ -4,10 +4,11 @@ var playButton = $('.action-play');
 var pauseButton = $('.action-pause');
 var rewindButton = $('.action-rwd');
 var forwardButton = $('.action-fwd');
+var seekMain = $('.seek-main');
 
 var path = "music_storage/sw/hand/"
-var audioSource = ["hand.m4a","3.m4a"];
-var playlistPos = 1; // Position in the current playlist
+var audioSource = ["hand.m4a","3.m4a","#9.m4a","routine.m4a"];
+var playlistPos = 3; // Position in the current playlist
 var minutes = 0; //Current song position minutes figure
 var seconds = 0; // Current song position seconds figure
 var duration; // Set up var for duration of current song
@@ -17,6 +18,19 @@ var music = new Howl({
 	src: [path + audioSource[playlistPos]]
 })
 
+// test to see how many hardware channels we can output to
+// if it's 6 or larger, we can play a 5.1 audio stream!
+if (Howler.ctx.destination.maxChannelCount >= 6) {
+	Howler.ctx.destination.channelCount = 6;
+}
+// otherwise, let's down-mix to 2.0
+else {
+	Howler.ctx.destination.channelCount = 2;
+}
+Howler.ctx.destination.channelCountMode = "explicit";
+Howler.ctx.destination.channelInterpretation = "discrete";
+
+console.log(Howler.ctx.destination.channelCount);
 
 pauseButton.hide();
 playButton.click(function(){
@@ -34,13 +48,15 @@ pauseButton.click(function(){
 
 music.on('play', function(){
 	timer = setInterval(function(){
-		var playbackPos = Math.floor(music.pos());
+		var playbackPos = Math.floor(music.seek());
 		minutes = Math.floor(playbackPos / 60);
 		seconds = playbackPos % 60;
 		if (seconds < 10){
 			seconds = "0" + seconds;
 		}
 		$(".now_playing-elapsed").text(minutes + ":" + seconds);
+		seekMain.val(playbackPos);
+		console.log(seekMain.val())
 	}, 200);
 })
 music.on('pause',function(){
@@ -48,20 +64,35 @@ music.on('pause',function(){
 });
 
 music.on('load', function(){
-	duration = music._duration;
+	duration = music.duration();
+	seekMain.prop({'max':duration});
 	$('.now_playing-total').text(convertToHumanTime(duration));
-
+	console.log(duration);
 });
 
 function convertToHumanTime(time){
 	Cminutes = Math.floor(time / 60);
 	Cseconds = Math.floor(time % 60);
+	if (Cseconds < 10){
+			Cseconds = "0" + Cseconds;
+		}
 	return(Cminutes + ":" + Cseconds);
 }
+
+
+seekMain.on('change', function(){
+	var seekTime = $(this).val();
+	music.seek(seekTime);
+});
+
+seekMain.slider();
+
 
 function skipTrack(direction){
 
 }
+
+
 
 
 });
