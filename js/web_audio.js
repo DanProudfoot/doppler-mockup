d579e1735c51b65ce929b3c1805c452fe69d7bf5
Dan Proudfoot <dan.proudfoot.iw@gmail.com>
2015-05-05 03:42:52 +0100

    Making the song changer

--- js/web_audio.js
+++ js/web_audio.js
@@ -8,16 +8,8 @@ var seekMain = $('.seek-holder');
 
 var path = "music_storage/sw/hand/"
 var audioSource = ["hand.m4a","3.m4a","9.m4a","routine.m4a"];
-var playlistPos = 2; // Position in the current playlist
-var minutes = 0; //Current song position minutes figure
-var seconds = 0; // Current song position seconds figure
-var duration; // Set up var for duration of current song
-var timer; // Setup up for actually tracking the duration of the song 
-var playbackPos; // Set up absolute position of playback
+//var playlistPos = 2; // Position in the current playlist
 
-var music = new Howl({
-	src: [path + audioSource[playlistPos]],
-});
 
 // test to see how many hardware channels we can output to
 // if it's 6 or larger, we can play a 5.1 audio stream!
@@ -34,53 +26,47 @@ Howler.ctx.destination.channelInterpretation = "discrete";
 //console.log(Howler.ctx.destination.maxChannelCount); // Log available output channels. Firefox just doesn't heed this.
 
 pauseButton.hide();
-playButton.click(function(){
-	id = music.play();
-	playButton.hide();
-	pauseButton.show();
 
-});
-pauseButton.click(function(){
-	music.pause();
-	pauseButton.hide();
-	playButton.show();
-});
+// var music0 = new Howl({
+// 	src: [path + audioSource[3]],
+// 	preload: true,
+// });
+
+// var music1 = new Howl({
+// 	src: [path + audioSource[0]],
+// 	preload: false,
+// });
+
 
 // The actual stuff
+
+
+var Song = function(playlistIndex){
+	var setSong = new Howl({
+		src: [path + audioSource[playlistIndex]],
+		preload:true
+	})
+	var stopSong = function(){
+		setSong.stop();
+	}
+	return {
+		setSong: setSong,
+		stopSong: stopSong
+	}
+}
+
+var currentSong = Song(2).setSong;
+
+$('header').click(function(){
+	var currentSong = Song(2).stopSong;
+	console.log("HEader");
+})
+
 
 var loadPercentage = setInterval(function(){
 	console.log(Math.floor(loadPerc*100) + "%");
 	$('.load_progress').css("width", Math.floor(loadPerc*100) + "%")
 }, 100);
-
-music.on('play', function(){
-	timer = setInterval(function(){
-		playbackPos = Math.floor(music.seek());
-		minutes = Math.floor(playbackPos / 60);
-		seconds = playbackPos % 60;
-		if (seconds < 10){
-			seconds = "0" + seconds;
-		}
-		$(".now_playing-elapsed").text(minutes + ":" + seconds);
-		seekMain.slider("value", playbackPos);
-	}, 200);
-})
-music.on('pause',function(){
-	clearInterval(timer);
-});
-
-music.on('load', function(){
-	duration = music.duration();
-	seekMain.prop({'max':duration});
-	$('.now_playing-total').text(convertToHumanTime(duration));
-	clearInterval(loadPercentage);
-	seekMain.slider({
-		max: duration,
-		min: 0,
-		step:1,
-		value:0
-	});
-});
 
 function convertToHumanTime(time){
 	Cminutes = Math.floor(time / 60);
@@ -91,9 +77,67 @@ function convertToHumanTime(time){
 	return(Cminutes + ":" + Cseconds);
 }
 
-seekMain.on('slide', function(event, ui){
-	music.seek(ui.value);
+currentSong.on('load', function(){
+
+	var minutes = 0; //Current song position minutes figure
+	var seconds = 0; // Current song position seconds figure
+	var duration; // Set up var for duration of current song
+	var timer; // Setup up for actually tracking the duration of the song 
+	var playbackPos; // Set up absolute position of playback
+
+	playButton.click(function(){
+		id = currentSong.play();
+		playButton.hide();
+		pauseButton.show();
+
+	});
+	pauseButton.click(function(){
+		currentSong.pause();
+		pauseButton.hide();
+		playButton.show();
+	});
+	rewindButton.click(function(){
+
+	});
+	forwardButton.click(function(){
+
+	});
+
+	duration = currentSong.duration();
+
+	$('.now_playing-total').text(convertToHumanTime(duration));
+	
+	clearInterval(loadPercentage);
+	
+	seekMain.slider({
+		max: duration,
+		min: 0,
+		step:1,
+		value:0
+	});
+
+	seekMain.on('slide', function(event, ui){
+		currentSong.seek(ui.value);
+	});
+
+	currentSong.on('play', function(){
+		timer = setInterval(function(){
+			playbackPos = Math.floor(currentSong.seek());
+			minutes = Math.floor(playbackPos / 60);
+			seconds = playbackPos % 60;
+			if (seconds < 10){
+				seconds = "0" + seconds;
+			}
+			$(".now_playing-elapsed").text(minutes + ":" + seconds);
+			seekMain.slider("value", playbackPos);
+		}, 200);
+	});
+
 });
+
+
+
+
 
 
 });
